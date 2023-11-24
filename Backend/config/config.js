a82b5689f742dfc57cac7f6734fa4e40cdc0f71c
learnacadman <105061140+learnacadman@users.noreply.github.com>
2023-11-24 22:23:07 +0530

    Create config.js

---
+++ Backend/config/config.js
@@ -0,0 +1,26 @@
+// sendgrid key
+exports.Sendgrid="somerandomvalue";
+
+// mongo database
+exports.mongo = "mongodb+srv://username:password@clustername.78ngidn.mongodb.net/?retryWrites=true&w=majority";
+
+
+// jwt authentication 
+exports.accessToken="somesecret";
+exports.refereshToken="somesecret";
+
+exports.accessTokenLife=100000;
+exports.refereshTokenLife=1000000;
+
+
+//Redis
+exports.redisHost = "somerandomvalue";
+exports.redisPassword="somerandomvalue";
+exports.redisPort="8000"
+
+// google auth
+exports.googleAuth = "somerandomvalue";
+
+//stripe payment
+exports.stripePayment = "sk_test_ADummyKey_GhB9kqCMm28ORyZXokMjR0LS8J5gYlx03E9QHtZLB9way700ZvSUeX3F";
+
